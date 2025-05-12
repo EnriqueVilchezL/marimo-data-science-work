f702fc04b954eaa956a600774fc80e28a435b8b5
abo-tresol <antonio1dbo@gmail.com>
2025-05-11 18:49:49 -0600

    load data

--- main.py
+++ main.py
@@ -7,6 +7,36 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _():
+    import polars as pl
+
+    return (pl,)
+
+
+@app.cell
+def _(pl):
+    dataset_filepath = "data/bots_vs_users.csv"
+    bot_dataset = pl.read_csv(dataset_filepath, null_values=["Unknown"])
+    return (bot_dataset,)
+
+
+@app.cell
+def _(bot_dataset, mo):
+    _df = mo.sql(
+        f"""
+        SELECT * FROM bot_dataset
+        """
+    )
+    return
+
+
+@app.cell
+def _():
+    import altair as alt
     return
 
 
